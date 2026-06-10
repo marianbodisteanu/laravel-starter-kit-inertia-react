cf7bbe913812254abe175269a74288e372c57a83
Marian Bodisteanu <279429649+marianbodisteanu@users.noreply.github.com>
2026-06-10 15:57:13 +0300

    fix: add rel=noreferrer to external links
    target=_blank without rel lets the target page control the opener
    tab via window.opener.
    
    Fixes react-doctor/jsx-no-target-blank.

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -70,6 +70,7 @@ export default function Welcome({
                                         <a
                                             href="https://laravel.com/docs"
                                             target="_blank"
+                                            rel="noreferrer"
                                             className="ml-1 inline-flex items-center space-x-1 font-medium text-[#f53003] underline underline-offset-4 dark:text-[#FF4433]"
                                         >
                                             <span>Documentation</span>
@@ -101,6 +102,7 @@ export default function Welcome({
                                         <a
                                             href="https://laracasts.com"
                                             target="_blank"
+                                            rel="noreferrer"
                                             className="ml-1 inline-flex items-center space-x-1 font-medium text-[#f53003] underline underline-offset-4 dark:text-[#FF4433]"
                                         >
                                             <span>Laracasts</span>
@@ -127,6 +129,7 @@ export default function Welcome({
                                     <a
                                         href="https://cloud.laravel.com"
                                         target="_blank"
+                                        rel="noreferrer"
                                         className="inline-block rounded-sm border border-black bg-[#1b1b18] px-5 py-1.5 text-sm leading-normal text-white hover:border-black hover:bg-black dark:border-[#eeeeec] dark:bg-[#eeeeec] dark:text-[#1C1C1A] dark:hover:border-white dark:hover:bg-white"
                                     >
                                         Deploy now
